9b6f61198cc9424e598cf36ebe8d66ca49ca89e7
GustavoHenriqueMuller <gustavo.henrique2015@yahoo.com.br>
2022-12-04 13:49:48 -0300

    Commit

---
+++ rpscv/gui.py
@@ -0,0 +1,149 @@
+import sys
+import numpy as np
+
+import cv2
+import pygame as pg
+import pygame.freetype
+
+class RPSGUI():
+
+    def __init__(self, loop = False):
+        pg.init()
+
+        self.loop = loop
+        self.sWidth = 640
+        self.sHeight = 480
+        self.surf = pg.display.set_mode((self.sWidth, self.sHeight))
+        pg.display.set_caption('Rock-Paper-Scissors by drgfreeman@tuta.io')
+        self.playerScore = 0
+        self.computerScore = 0
+        self.playerImage = pg.Surface((200, 300))
+        self.computerImage = pg.Surface((200, 300))
+        self.playerImagePosition = (60, 160)
+        self.computerImagePosition = (380, 160)
+        self.playerZone = pg.Surface((250, 330))
+        self.computerZone = pg.Surface((250, 330))
+        self.playerZonePosition = (35, 145)
+        self.computerZonePosition = (355, 145)
+        self.winner = None
+
+        # colors
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.RED = (255, 0, 0)
+        self.GREEN = (0, 255, 0)
+        self.BLUE = (0, 0, 255)
+
+    def blitTextAlignCenter(self, surf, text, pos):
+        tWidth = text[1].width
+        surf.blit(text[0], (pos[0] - tWidth / 2, pos[1]))
+
+    def draw(self):
+        # Fill surface with background color
+        self.surf.fill(self.WHITE)
+
+        # Draw boxes around computer and player areas
+        playerVertices = [(5, 3), (315, 3), (315, 476), (5, 476), (5, 3)]
+        pg.draw.polygon(self.surf, self.BLACK, playerVertices, 1)
+
+        computerVertices = [(325, 3), (634, 3), (634, 476), (325, 476), (325, 3)]
+        pg.draw.polygon(self.surf, self.BLACK, computerVertices, 1)
+
+        # Render computer and player text
+        font = pg.freetype.SysFont(None, 30)
+        text = font.render('PLAYER', self.BLACK)
+        self.blitTextAlignCenter(self.surf, text, (160,15))
+        text = font.render('COMPUTER', self.BLACK)
+        self.blitTextAlignCenter(self.surf, text, (480,15))
+
+        # Set computer and player zone colors
+        if self.winner == 'player':
+            self.playerZone.fill(self.GREEN)
+            self.computerZone.fill(self.RED)
+        elif self.winner == 'computer':
+            self.playerZone.fill(self.RED)
+            self.computerZone.fill(self.GREEN)
+        elif self.winner == 'tie':
+            self.playerZone.fill(self.BLUE)
+            self.computerZone.fill(self.BLUE)
+        else:
+            self.playerZone.fill(self.WHITE)
+            self.computerZone.fill(self.WHITE)
+
+        # Blit computer and player zone
+        self.surf.blit(self.playerZone, self.playerZonePosition)
+        self.surf.blit(self.computerZone, self.computerZonePosition)
+
+        # Blit computer and player images
+        self.surf.blit(self.playerImage, self.playerImagePosition)
+        self.surf.blit(self.computerImage, self.computerImagePosition)
+
+        # Render computer and player scores
+        font = pg.freetype.SysFont(None, 100)
+        text = font.render(str(self.playerScore), self.BLACK)
+        self.blitTextAlignCenter(self.surf, text, (160, 60))
+        text = font.render(str(self.computerScore), self.BLACK)
+        self.blitTextAlignCenter(self.surf, text, (480, 60))
+
+    def gameOver(self, delay = 3500):
+        # Create surface for Game Over message
+        goZone = pg.Surface((400, 200))
+
+        # Fill surface with background color
+        goZone.fill(self.WHITE)
+
+        # Draw box around surface
+        vertices = [(3, 3), (396, 3), (396, 196), (3, 196), (3, 3)]
+        pg.draw.polygon(goZone, self.BLACK, vertices, 1)
+
+        # Render text on surface
+        font = pg.freetype.SysFont(None, 40)
+        gameOverText = font.render('GAME OVER', self.BLACK)
+        self.blitTextAlignCenter(goZone, gameOverText, (200, 45))
+
+        if self.playerScore > self.computerScore:
+            winner = 'PLAYER'
+            color = self.GREEN
+        else:
+            winner = 'COMPUTER'
+            color = self.RED
+
+        winnerText = font.render('{} WINS!'.format(winner), color)
+        self.blitTextAlignCenter(goZone, winnerText, (200, 110))
+
+        # Blit goZone to main surface
+        pos = (self.sWidth / 2 - 200, 175)
+        self.surf.blit(goZone, pos)
+
+        pg.display.flip()
+        pg.time.wait(delay)
+
+        if self.loop:
+            self.reset()
+        else:
+            self.quit()
+
+    def quit(self, delay = 0):
+        pg.time.wait(delay)
+        pg.quit()
+        sys.exit()
+
+    def reset(self):
+        self.playerScore = 0
+        self.computerScore = 0
+
+    def setComputerImage(self, img):
+        self.computerImage = pg.surfarray.make_surface(img[:,::-1,:])
+
+    def setPlayerImage(self, img):
+        self.playerImage = pg.surfarray.make_surface(img[::-1,:,:])
+
+    def setWinner(self, winner):
+        self.winner = winner
+
+        if winner == 'player':
+            self.playerScore += 1
+        elif winner == 'computer':
+            self.computerScore += 1
+
+        
---
+++ train.py
@@ -0,0 +1,137 @@
+import sys
+import numpy as np
+
+# Random State
+rs = 42
+
+# Classifier output .pkl filename
+pklFilename = 'clf.pkl'
+
+# Number of folds of Stratified KFold cross-validation
+n_splits = 5
+
+# Grid Search parameters
+pca__n_components = [40] # Number of components of Principal Component Analysis
+clf__gamma = np.logspace(-4, -2, 3) # [.0001, .001, .01]
+clf__C = np.logspace(0, 2, 3) # [1, 10, 100]
+scoring = 'f1_micro'
+
+# The n_jobs parameter controls the number of CPU cores to use in parallel for
+# training the machine learning model.
+n_jobs = 1
+
+def train(nbImg = 0, showCvScore = True):
+    import time
+    t0 = time.time()
+
+    def dt():
+        return round(time.time() - t0, 2)
+
+    print('+{}s: Importing libraries'.format(dt()))
+
+    import pickle
+
+    from sklearn.pipeline import Pipeline
+    from sklearn.decomposition import PCA
+    from sklearn.model_selection import StratifiedShuffleSplit
+    from sklearn.model_selection import StratifiedKFold
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.svm import SVC
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import classification_report
+
+    from rpscv import imgproc as imp
+    from rpscv import utils
+
+    # Generate image data from stored images
+    print('+{}s: Generating image data'.format(dt()))
+    features, labels = imp.generateGrayFeatures(nbImg = nbImg, verbose = False, rs = rs)
+
+    unique, count = np.unique(labels, return_counts = True)
+
+    # Print the number of traning images for each label
+    for i, label in enumerate(unique):
+        print('  {}: {} images'.format(utils.gestureTxt[label], count[i]))
+
+    # Generate test set
+    print('+{}s: Generating test set'.format(dt()))
+    sssplit = StratifiedShuffleSplit(n_splits = 1, test_size = .15, random_state = rs)
+    for train_index, test_index in sssplit.split(features, labels):
+        features_train = features[train_index]
+        features_test = features[test_index]
+        labels_train = labels[train_index]
+        labels_test = labels[test_index]
+
+    # Define pipeline parameters
+    print('+{}s: Defining pipeline'.format(dt()))
+    steps = [('pca', PCA()), ('clf', SVC(kernel = 'rbf'))]
+    pipe = Pipeline(steps)
+
+    # Define cross-validation parameters
+    print('+{}s: Defining cross-validation'.format(dt()))
+    cv = StratifiedKFold(n_splits = n_splits, shuffle = True, random_state = rs)
+
+    # Define grid-search parameters
+    print('+{}s: Defining grid search'.format(dt()))
+    grid_params = dict(pca__n_components = pca__n_components,
+                       clf__gamma = clf__gamma,
+                       clf__C = clf__C)
+					   
+    grid = GridSearchCV(pipe, grid_params, scoring = scoring, n_jobs=n_jobs, refit = True, cv = cv, verbose = 1)
+
+    print('Grid search parameters:')
+    print(grid)
+
+    # Fit the classifier
+    t0_train = time.time()
+    print('+{}s: Fitting classifier'.format(dt()))
+    grid.fit(features_train, labels_train)
+    dt_train = time.time() - t0_train
+
+    if showCvScore:
+        # Print the results of the grid search cross-validation
+        cvres = grid.cv_results_
+        print('Cross-validation results:')
+        for score, std, params in zip(cvres['mean_test_score'],
+                cvres['std_test_score'], cvres['params']):
+            print('  {}, {}, {}'.format(round(score, 4), round(std, 5), params))
+
+    # Print the best score and best parameters from the grid-search
+    print('Grid search best score: {}'.format(grid.best_score_))
+    print('Grid search best parameters:')
+    for key, value in grid.best_params_.items():
+        print('  {}: {}'.format(key, value))
+
+    # Validate classifier on test set
+    print('+{}s: Validating classifier on test set'.format(dt()))
+    pred = grid.predict(features_test)
+    score = f1_score(labels_test, pred, average='micro')
+    print('Classifier f1-score on test set: {}'.format(score))
+    print('Confusion matrix:')
+    print(confusion_matrix(labels_test, pred))
+    print('Classification report:')
+    tn = [utils.gestureTxt[i] for i in range(3)]
+    print(classification_report(labels_test, pred, target_names=tn))
+
+    # Write classifier to a .pkl file
+    print('+{}s: Writing classifier to {}'.format(dt(), pklFilename))
+    with open(pklFilename, 'wb') as f:
+        f.flush()
+        pickle.dump(grid, f)
+
+    print('+{}s: Done!'.format(dt()))
+
+    return grid.best_score_, score, dt_train
+
+if __name__ == '__main__':
+    # Read command line arguments
+    argv = sys.argv
+    showCvScore = True
+
+    if len(sys.argv) > 1:
+        for arg in argv[1:]:
+            if arg == '--no-cv-score':
+                showCvScore = False
+
+    train(showCvScore = showCvScore)
